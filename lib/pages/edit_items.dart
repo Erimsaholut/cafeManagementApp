6db10428c99ffbc56679eaeb88a84e6e5015c603
Erimsah Olut <erimsaholut@icloud.com>
2024-02-02 21:45:59 +0300

    alertDialogs for editItemsPage

--- lib/pages/edit_items.dart
+++ lib/pages/edit_items.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import '../datas/menu_data/read_data.dart';
+import '../utils/custom_alert_button.dart';
 
 class EditItems extends StatelessWidget {
   EditItems({super.key});
+
   ReadData readData = ReadData();
 
   @override
@@ -14,7 +16,6 @@ class EditItems extends StatelessWidget {
       ),
       body: Container(
         color: Colors.tealAccent,
-        padding: const EdgeInsets.all(16.0),
         child: FutureBuilder<Map<String, dynamic>?>(
           future: readData.getRawData(),
           builder: (context, snapshot) {
@@ -28,20 +29,72 @@ class EditItems extends StatelessWidget {
               List<dynamic> menuItems = snapshot.data!['menu'];
 
               return GridView.count(
-                crossAxisCount: 4, // Set the number of columns
+                crossAxisCount: 4,
                 crossAxisSpacing: 16.0,
                 mainAxisSpacing: 16.0,
                 children: menuItems.map((item) {
                   return TextButton(
                     onPressed: () {
-
-                      print(item['name']);
-                      
+                      showDialog(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return AlertDialog(
+                            title: Text("Edit Item"),
+                            content: SingleChildScrollView(
+                              child: Column(
+                                children: [
+                                  TextField(
+                                    decoration:
+                                        InputDecoration(labelText: 'Name'),
+                                    // Burada kullanıcıdan isim alıyoruz.
+                                  ),
+                                  TextField(
+                                    decoration:
+                                        InputDecoration(labelText: 'Price'),
+                                    keyboardType: TextInputType.number,
+                                    // Burada kullanıcıdan fiyat alıyoruz.
+                                  ),
+                                  SizedBox(height: 16.0),
+                                  Text("Ingredients:"),
+                                  // Burada item["ingredients"] listesindeki eleman sayısı kadar checkbox oluşturuyoruz.
+                                  ...List.generate(
+                                    item['ingredients'].length,
+                                    (index) => CheckboxListTile(
+                                      title: Text(item['ingredients'][index]),
+                                      value: false,
+                                      // Checkbox başlangıç değeri, kullanıcı seçim yapacak.
+                                      onChanged: (bool? value) {
+                                        // Checkbox değeri değiştiğinde burada işlemler yapılabilir.
+                                      },
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            actions: [
+                              TextButton(
+                                onPressed: () {
+                                  Navigator.of(context).pop();
+                                },
+                                child: Text('Cancel'),
+                              ),
+                              TextButton(
+                                onPressed: () {
+                                  // Burada kullanıcının girdiği değerleri kullanabilirsiniz.
+                                  // Örneğin: nameController.text, priceController.text, selectedIngredients
+                                  Navigator.of(context).pop();
+                                },
+                                child: Text('Save'),
+                              ),
+                            ],
+                          );
+                        },
+                      );
                     },
                     style: ButtonStyle(
                       tapTargetSize: MaterialTapTargetSize.shrinkWrap,
                       overlayColor: MaterialStateProperty.resolveWith<Color?>(
-                            (Set<MaterialState> states) {
+                        (Set<MaterialState> states) {
                           if (states.contains(MaterialState.pressed)) {
                             return Colors.transparent;
                           }
